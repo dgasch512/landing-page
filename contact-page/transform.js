6c4ec00698a82f0f65aec0faa2c350e42240b646
dgasch512 <gaschdav208@gmail.com>
2020-10-02 23:16:15 -0500

    added contacts page

---
+++ contact-page/transform.js
@@ -0,0 +1,13 @@
+$(".address-icon").hover(function() {
+  $('.grid').toggleClass('grid-transform-address');
+});
+$(".phone i").hover(function() {
+  $('.phone i').toggleClass('transform-phone');
+});
+$(".email i").hover(function() {
+  $('.email i').toggleClass('transform-email');
+});
+$("#git").hover(function() {
+  $('.git').toggleClass('transform-git');
+});
+
